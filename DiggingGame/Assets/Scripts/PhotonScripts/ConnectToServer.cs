a898c613e7cdb4d70585d74c86314e1b428b570b
Andrea Swihart <andreaswihartdecoster@gmail.com>
2022-09-29 16:56:28 -0500

    Began Initial Photon Setup

---
+++ DiggingGame/Assets/Scripts/PhotonScripts/ConnectToServer.cs
@@ -0,0 +1,44 @@
+/*****************************************************************************
+// File Name :         ConnectToServer.cs
+// Author :            Andrea Swihart-DeCoster
+// Creation Date :     September 29th, 2022
+//
+// Brief Description : This document controls the connecting and joining 
+                       process for joining a Photon server.
+*****************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+// MonoBehaviourPunCallbacks gives access to the callback functions which
+// automatically happens when specific events occur.
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Connects to photon server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    /// <summary>
+    /// Callbaack function to check when successfully connected to server.
+    /// The player then joins the lobby.
+    /// </summary>
+    public override void OnConnectedToMaster()
+    {
+        // Allows for joining and creating rooms later on
+        PhotonNetwork.JoinLobby();
+    }
+
+    /// <summary>
+    /// Loads lobby scene once connected
+    /// </summary>
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
