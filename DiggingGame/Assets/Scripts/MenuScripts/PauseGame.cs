2e11fc8421bfee5da869b2e41a0b4cc57f06168d
Rudy <rwolfer8@gmail.com>
2022-10-27 15:37:36 -0500

    Pause Functionality
    Game can be paused and has stuff now.

---
+++ DiggingGame/Assets/Scripts/MenuScripts/PauseGame.cs
@@ -0,0 +1,51 @@
+/*****************************************************************************
+// File Name :         PauseGame.cs
+// Author :            Rudy Wolfer
+// Creation Date :     October 27th, 2022
+//
+// Brief Description : Simple script for Pause Menu functionality.
+*****************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseScreen;
+
+    private void Start()
+    {
+        _pauseScreen.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        _pauseScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        _pauseScreen.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1.0f;
+        Application.Quit();
+    }
+
+    public void Rules()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/1xR1zlYT4rUaA1m1OP98pxGKvQqt7RB9VVsK4ikHUPjc/edit?usp=sharing");
+    }
+}
