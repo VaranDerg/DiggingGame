afe852b66c88a96a32f5ea62ee7d6f2b773c8fb4
Rudy <rwolfer8@gmail.com>
2022-11-07 16:35:19 -0600

    Audio Script Touchups

--- DiggingGame/Assets/Scripts/OtherScripts/AudioManager.cs
+++ DiggingGame/Assets/Scripts/OtherScripts/AudioManager.cs
@@ -40,11 +40,6 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        Play("BackgroundMusic");
-    }
-
     public void Play(string name)
     {
        Sound s = Array.Find(sounds, sound => sound.name == name);
